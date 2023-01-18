0780a74e11004b8922f5c73245ed732eca13fad6
Egidijus <vaitkeviciusegis@gmail.com>
2023-01-18 23:57:42 +0200

    Exercise login screen

--- AutoLayoutLogin/ViewController.swift
+++ AutoLayoutLogin/ViewController.swift
@@ -13,7 +13,19 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+//Exercise: Login screen
+//1. Take "AutoLayoutLogin_Start" from class resources
+//2. Logo should have 8pt from top, 16pt from left and right
+//3. Logo height should be 25% of view height
+//A
+//4. Login and Password text fields should be centered vertically in a view (not one of them but both of them). They should also be center horizontally.
+//5. Login and Password text fields should be 80% of view width
+//6. There should be 16pt space between Login and Password
+//7. Forgot password button should be 16pt from the bottom, centered horizonally in a view
+//8. Login button bottom should be 16pt distance from Forgot password button
+//9. Login button should be centered horizontally with Forgot password button
+//CodeAcademy
+//CodeAcademy
 
 }
 
